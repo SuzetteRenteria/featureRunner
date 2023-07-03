d65ac676913d33577e37504b4b28699ffb39ecd0
Suzette Renteria <sgr68@txstate.edu>
2023-07-03 09:29:17 -0500

    Removed Clicker A, fixed the Click and Timer TV (it wasn't displaying zero onFinish of timer), and fixed button layout on UI (it was bunched up before)

--- app/src/main/java/com/example/jokesapp/ClickerGameActivity.java
+++ app/src/main/java/com/example/jokesapp/ClickerGameActivity.java
@@ -2,6 +2,7 @@ package com.example.jokesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -17,14 +18,16 @@ import android.widget.TextView;
 import com.example.jokesapp.R;
 
 public class ClickerGameActivity extends AppCompatActivity {
-    Button clkrBtnA;
     Button clkrBtnB;
     Button clkrStartBtn;
+    Button clkrReset;
     TextView tv_ClksLeft;
     TextView tv_clkrTimer;
     ProgressBar roundProgBar;
-    int clksCntr = 0; // clicks counter
-    int secsLim = 20; // starting timer
+    static final int clksCntr_DEFAULT = 0;
+    int clksCntr = clksCntr_DEFAULT; // clicks counter
+    static final int secsLim_DEFAULT = 20; // for this game I decided 20 sec range
+    int secsLim = secsLim_DEFAULT; // starting timer
     private int highScore = 0; // track high score
 
     public void popUpWin(){
@@ -46,72 +49,85 @@ public class ClickerGameActivity extends AppCompatActivity {
 
     public int decSecsLim(){
         return secsLim--;
+    } // decrement timer val to 0
+
+    public int setToDefclksCntr(){ return clksCntr = clksCntr_DEFAULT; }
+    public int setToDefsecsLim(){ return secsLim = secsLim_DEFAULT; }
+    public int getclksCntr(){
+        return clksCntr;
     }
+    public int getSecsLim(){
+        return secsLim;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clicker_game);
 
-        clkrBtnA = (Button) findViewById(R.id.clkrBtnA);
         clkrBtnB = (Button) findViewById(R.id.clkrBtnB);
         clkrStartBtn = (Button) findViewById(R.id.clkrStartBtn);
+        clkrReset = (Button) findViewById(R.id.resetBtn);
         tv_ClksLeft = (TextView) findViewById(R.id.tv_ClksLeft);
         tv_clkrTimer = (TextView) findViewById(R.id.tv_clkrTimer);
         roundProgBar = (ProgressBar) findViewById(R.id.roundProgBar);
-        final CountDownTimer tmr = new CountDownTimer(20*1000, 1000) {
+        final CountDownTimer tmr = new CountDownTimer(secsLim_DEFAULT * 1000, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
-                int t = decSecsLim();
-                String t1 = getString(R.string.timeLeft);
-                roundProgBar.setProgress(t * 5);
+                //int t = decSecsLim();
+                roundProgBar.setProgress(decSecsLim() * 5);
                 roundProgBar.setMax(100);
-                tv_clkrTimer.setText(t1 + Integer.toString(t));
+                tv_clkrTimer.setText(getString(R.string.timerLeft_msg, getSecsLim()));
             }
 
             @Override
             public void onFinish() {
-                clkrBtnA.setEnabled(false);
                 clkrBtnB.setEnabled(false);
                 roundProgBar.setProgress(0);
-                //add win
-                popUpWin();
+                tv_ClksLeft.setText(getString(R.string.clksLeft_msgg, getclksCntr()));
+                tv_clkrTimer.setText(getString(R.string.timerLeft_msg, getSecsLim()));
+                popUpWin(); // popup window
             }
         };
 
         clkrStartBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                clkrBtnA.setEnabled(true);
                 clkrBtnB.setEnabled(true);
                 clkrStartBtn.setEnabled(false);
                 clksCntr = 0;
-                secsLim = 20;
+                secsLim = secsLim_DEFAULT;
                 tmr.start();
             }
+            /*
             public void onFinish(View v){
                 // game over
-                clkrBtnA.setEnabled(false);
                 clkrBtnB.setEnabled(false);
                 String t1 = getString(R.string.clkrOVER);
                 tv_ClksLeft.setText(t1 + Integer.toString(clksCntr));
             }
+
+             */
         });
 
-        Thread tT2 = new Thread(new Runnable() {
+
+
+        clkrReset.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                clkrBtnA.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        //clksCntr++;
-                        String t2 = getString(R.string.tv_clksLeft);
-                        int it2 = incClksCntr();
-                        tv_ClksLeft.setText(t2 + Integer.toString(it2));
-                    }
-                });
+            public void onClick(View v) {
+                tmr.cancel();
+
+                clkrStartBtn.setEnabled(true);
+                setToDefclksCntr();
+                setToDefsecsLim();
+
+                tv_ClksLeft.setText(getString(R.string.clksLeft_msgg, getclksCntr()));
+                tv_clkrTimer.setText(getString(R.string.timerLeft_msg, getSecsLim()));
+                roundProgBar.setProgress(0);
             }
         });
+
 
         Thread tT = new Thread(new Runnable() {
             @Override
@@ -121,27 +137,15 @@ public class ClickerGameActivity extends AppCompatActivity {
                     public void onClick(View v) {
                         //clksCntr++;
                         //incClksCntr(clksCntr);
-                        String t3 = getString(R.string.tv_clksLeft);
-                        int it3 = incClksCntr();
-                        tv_ClksLeft.setText(t3 + Integer.toString(it3));
+                        //String t3 = getString(R.string.tv_clksLeft);
+                        //int it3 = incClksCntr();
+                        tv_ClksLeft.setText(getString(R.string.clksLeft_msgg, incClksCntr()));
                     }
                 });
             }
-        });
+        }); // Button B thread
 
-        tT2.start();
         tT.start();
-
-        try {
-            tT2.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            tT.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
 
     }
 }
