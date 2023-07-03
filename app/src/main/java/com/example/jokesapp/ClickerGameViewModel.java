4b92a3ff4621eab0a6807def663d1f9d453a6241
Suzette Renteria <sgr68@txstate.edu>
2023-07-03 09:34:10 -0500

    Removed Clicker A, fixed the Click and Timer TV (it wasn't displaying zero onFinish of timer), and fixed button layout on UI (it was bunched up before)

---
+++ app/src/main/java/com/example/jokesapp/ClickerGameViewModel.java
@@ -0,0 +1,8 @@
+package com.example.jokesapp;
+
+import androidx.lifecycle.ViewModel;
+
+public class ClickerGameViewModel extends ViewModel {
+    //TODO: SEPARATE DATA FROM UI WITH VIEWMODEL FOR CLICKER GAME
+
+}
